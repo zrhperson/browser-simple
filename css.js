3a40bb824e48fb63ea5c7f745ec293c45e7ee319
zhouruihao <ruihao.zhou@2339.com>
2021-03-25 18:37:58 +0800

    css

--- css.js
+++ css.js
@@ -29,6 +29,32 @@ function match(element, selector) {
 
 }
 
+function specificity(selector) {
+    let p = [0,0,0,0];
+    let selectorParts = selector.split(' ');
+    for(let part of selectorParts) {
+        if(part.charAt('0') == '#'){
+            p[1] += 1;
+        }else if(part.charAt('0') == '.'){
+            p[2] += 1;
+        }else{
+            p[3] += 1;
+        }
+    }
+    return p;
+}
+
+function compare(sp1, sp2) {
+    if(sp1[0] - sp2[0]) {
+        return sp1[0] - sp2[0]
+    }else if(sp1[1] - sp2[1]) {
+        return sp1[1] - sp2[1]
+    }else if(sp1[2] - sp2[2]) {
+        return sp1[2] - sp2[2]
+    }
+    return sp1[3] - sp2[3]
+}
+
 function computeCss(element) {
     if(!element.computeStyle){
         element.computeStyle = {}
@@ -51,8 +77,20 @@ function computeCss(element) {
             matched = true;
         }
         if(matched){
-            console.log("Element====>", element);
-            console.log("matched====>", rule);
+            let sp = specificity(rule.selectors[0])
+            let computeStyle = element.computeStyle;
+            for(let declaration of rule.declarations){
+                if(!computeStyle[declaration.property]){
+                    computeStyle[declaration.property] = {}
+                }
+                if(!computeStyle[declaration.property].specificity){
+                    computeStyle[declaration.property].value = declaration.value;
+                    computeStyle[declaration.property].specificity = sp
+                }else if(compare(computeStyle[declaration.property].specificity, sp) < 0){
+                    computeStyle[declaration.property].specificity = sp
+                    computeStyle[declaration.property].value = declaration.value;
+                }
+            }
         }
     }
 }
